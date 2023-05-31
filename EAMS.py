7b9e5707a74aeeb66f0a5789e7729f9ea13b9d94
Brad Cunningham <brad87@gmail.com>
2023-05-31 12:13:46 -0400

    Working on functionality to launch tasks.

--- EAMS.py
+++ EAMS.py
@@ -22,8 +22,15 @@ def executeQuery(sql):
 
     cursor = connection.cursor(cursor_factory = psycopg2.extras.DictCursor)
     try:
+        
+        print(sql)
         cursor.execute(sql)
+
         if sql.strip().upper().startswith("UPDATE"):
+            connection.commit()
+            return None  # Return None for update queries
+        
+        if sql.strip().upper().startswith("INSERT"):
             connection.commit()
             return None  # Return None for update queries
         
@@ -39,6 +46,7 @@ def executeQuery(sql):
 def getEnabledEndpoints():
     sql = '''SELECT ID, DISPLAY_NAME, ADDRESS, STATUS FROM ENDPOINT WHERE ENABLED = TRUE;'''
     endpointList = executeQuery(sql)
+    print(endpointList)
     return endpointList
 
 def updateEndpointStatus(agentID, status):
@@ -106,18 +114,38 @@ def getWSGIEndpointStatus(endpointAddress):
 
 def createTaskEntry(scheduleID, instanceID, effectiveDate, endpointUserID, workingDir, serverCommand, serverCommandArgs):
     DEFAULT_STATUS = 'SCHEDULED'
-    sql = f'''INSERT INTO TASK (SCHEDULE_ID, INSTANCE_ID, STATUS ENDPOINT_USER_ID, WORKING_DIR, SERVER_COMMAND, SERVER_COMMAND_ARGS)
+    sql = f'''INSERT INTO TASK (SCHEDULE_ID, INSTANCE_ID, EFFECTIVE_DATE, STATUS, ENDPOINT_USER_ID, WORKING_DIR, SERVER_COMMAND, SERVER_COMMAND_ARGS)
               VALUES ({scheduleID}, {instanceID}, '{effectiveDate}', '{DEFAULT_STATUS}', {endpointUserID}, '{workingDir}', '{serverCommand}', '{serverCommandArgs}')
            '''
     executeQuery(sql)
 
 def getScheduleDefnDict(scheduleID):
 
-    sql = f'''SELECT SERVER_COMMAND, SERVER_COMMAND_ARGS, RUN_USER_ID, WORKING_DIR, CALENDAR_ID, ENDPOINT_ID WHERE SCHEDULE_ID = {scheduleID} AND ENABLED = True'''
-    scheduleDefnDict=executeQuery(sql)
+    sql = f'''SELECT SERVER_COMMAND, SERVER_COMMAND_ARGS, RUN_USER_ID, WORKING_DIR, CALENDAR_ID, ENDPOINT_ID FROM SCHEDULE_DEFN WHERE SCHEDULE_ID = {scheduleID} AND ENABLED = True'''
+    scheduleDefnDict = executeQuery(sql)
+
+    if scheduleDefnDict is None:
+        writeLogEntry('WARNING', f'getScheduleDefnDict: no schedule defn found for {scheduleID}')
+        return None
+
+    print(scheduleDefnDict[0]['server_command'])
+    #serverCommand = scheduleDefnDict[1]['server_command']
+    return scheduleDefnDict
+
+def getTaskDict(scheduleID, instanceID, EffectiveDate):
+    sql = f'''SELECT * FROM TASK 
+               WHERE SCHEDULE_ID = {scheduleID} 
+                 AND INSTANCE_ID = {instanceID} 
+                 AND EFFECTIVE_DATE = '{EffectiveDate};'''
+    taskDict = executeQuery(sql)
+    if taskDict is None:
+        return None
+
+    return taskDict[0]
+
 
 def getLastTaskStatusDict(scheduleID, effectiveDate):
-    sql = f'''SELECT INSTANCE_ID, STATUS FROM TASK WHERE DATE_TRUNC("day", EFFECTIVE_DATE) = {effectiveDate} WHERE SCHEDULE_ID = {scheduleID} '''
+    sql = f'''SELECT INSTANCE_ID, STATUS FROM TASK WHERE DATE_TRUNC('day', EFFECTIVE_DATE) = '{effectiveDate}' AND SCHEDULE_ID = {scheduleID} '''
     rawStatus = executeQuery(sql)   
     if rawStatus is None:
         #return tuple of (lastInstanceID, lastStatus)
@@ -130,22 +158,89 @@ def getLastTaskStatusDict(scheduleID, effectiveDate):
 
 def generateTasks(scheduleID, effectiveDate):
     lastTaskStatus = getLastTaskStatusDict(scheduleID, effectiveDate)
+    scheduleDefnDict = getScheduleDefnDict(scheduleID)
 
-    if lastTaskStatus['lastStatus'] in (0,'Success'):
-        print('do this next')
-
-
-
-
+    if scheduleDefnDict is None:
+        return
     
+    if lastTaskStatus['lastStatus'] in (0,'Success'):
+        lastInstanceID = lastTaskStatus['lastInstanceID']
+        nextInstanceID = lastInstanceID + 1
+    else: 
+        writeLogEntry('WARNING', f'generateTasks: Attempted to create a task where one already exists {scheduleID}')
+        return
+        
+    endpointUserID = scheduleDefnDict[0]['run_user_id']
+    workingDir = scheduleDefnDict[0]['working_dir']
+    serverCommand = scheduleDefnDict[0]['server_command']
+    serverCommandArgs = scheduleDefnDict[0]['server_command_args']
+
+    createTaskEntry(scheduleID, nextInstanceID, effectiveDate, endpointUserID, workingDir, serverCommand, serverCommandArgs)
+
+def getRunEligibleTasks():
+    
+    sql = '''SELECT SCHEDULE_ID, INSTANCE_ID FROM TASK WHERE STATUS IN ('SCHEDULED', 'WAITING ON DEPENDENCY', 'WAITING ON RESOURCE') AND  '''
+    eligibleTasks = executeQuery(sql)
+    runnableTasks = []
+
+    if eligibleTasks is None:
+        return None
+
+    for task in eligibleTasks: 
+        taskScheduleID = task['schedule_id']
+        taskInstanceID = task['instance_id']
+
+        selectOpenDepsByTaskIDSQL = f'''SELECT ID FROM TASK_DEPENDENCY WHERE TASK_SCHEDULE_ID = {taskScheduleID}, AND TASK_INSTANCE_ID = {taskInstanceID} AND STATUS LIKE 'OPEN';'''
+        openDeps = executeQuery(selectOpenDepsByTaskIDSQL)
+        if openDeps is None:
+            runnableTasks.append({"scheduleID": taskScheduleID, "instanceID": taskInstanceID})
+
+    return runnableTasks
+
+def getEndpointDict(endpointID):
+    sql = f'''SELECT * FROM ENDPOINT WHERE ID = {endpointID};'''
+    endpointDict = executeQuery(sql)
+
+    if endpointDict is None: 
+        writeLogEntry('WARNING', f'getEndpointDict: Couldn\'t look up endpoint with ID {endpointID}')
+        return None
+    return endpointDict[0]
+
+def launchTaskWSGI(taskDict, endpointDict):
+    print('Yup')
+
+
+def launchTask(scheduleID, instanceID, effectiveDate):
+    taskDict = getTaskDict(scheduleID, instanceID, effectiveDate)
+
+    if taskDict is None: 
+        writeLogEntry('WARNING', f'launchTask: Couldn\'t look up task with IDs {scheduleID}, {instanceID}, {effectiveDate}')
+        return None
+
+    taskEndpointID = taskDict['endpoint_id']
+    endpointDict = getEndpointDict(taskEndpointID)
+
+    if endpointDict is None:
+        writeLogEntry('WARNING', f'launchTask: Couldn\'t look up endpoint with ID {taskEndpointID}')
+        return None
+    
+    endpointType = endpointDict['endpoint_type']
+
+    if endpointType == 'wsgi':
+        launchTaskWSGI(taskDict, endpointDict)
+
+
+def TLMLoop():
+    while True:
+        tasksToLaunch = getRunEligibleTasks()
+
+        if tasksToLaunch is not None: 
+            print('yup')
+
 
 if __name__ == "__main__":
-    epStatusLoop = Process(target=endpointStatusLoop)
-    epStatusLoop.start()
-    app.run(debug=True, use_reloader=False, port=8080)
-    epStatusLoop.join()
-
-
-
-
-
+    #epStatusLoop = Process(target=endpointStatusLoop)
+    #epStatusLoop.start()
+   # app.run(debug=True, use_reloader=False, port=8080)
+    #epStatusLoop.join()
+    generateTasks(1, '2023-05-31')
